18fca255f7ffc4403e2583b67c495137700cddbf
John Galt <johngaltfirstrun@gmail.com>
2025-06-11 06:59:34 -0400

    vanilla: build/override Talkback and ESpeakNG

--- config/packages.mk
+++ config/packages.mk
@@ -86,10 +86,12 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 
 ifneq ($(TARGET_BUILD_GAPPS),true)
 PRODUCT_PACKAGES += \
+    ESpeakNG \
     Etar \
     LatinIME \
     messaging \
-    SetupWizard
+    SetupWizard \
+    Talkback
 endif
 
 # Include explicitly to work around GMS issues
